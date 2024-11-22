63852534524992f2a90a1ce9f680e8f93eeb7029
W-Sweet <liamsweet@me.com>
2024-11-22 17:20:37 -0500

    Finished select playlist implementation and random song selection

--- static/webpage.js
+++ static/webpage.js
@@ -1,15 +1,15 @@
-function buttonPressed(){
+function buttonPressed() {
     console.log("Button was pressed")
     alert('Button was pressed')
 }
 
-function change(){
+function change() {
     document.getElementById('here').innerHTML = "Changed Value"
 }
 
 document.getElementById('dataButton').addEventListener('click', function () {
     console.log("HERE")
-    fetch('/getPlaylistNames', { method: 'POST'}) // run all playlist names. 
+    fetch('/getPlaylistNames', { method: 'POST' }) // run all playlist names. 
         .then(response => response.json())
         .then(data => {
             console.log("PLAYLIST NAMES:", data);
@@ -25,53 +25,64 @@ document.getElementById('dataButton').addEventListener('click', function () {
                 toAdd = new Option(name, iter);
                 console.log("printing name again, ", toAdd.textContent)
                 select.options[select.options.length] = toAdd; // add to dropdown menu all possible playlists. 
-                iter+=1;
+                iter += 1;
             });
         })
-    .catch(error => console.error('Error:', error));
+        .catch(error => console.error('Error:', error));
 });
 
 document.getElementById('getSongs').addEventListener('click', function () {
     //before we get a random song from a playlist, we need the url for the playlist.
     console.log("Pressed second button");
-    var URLS = [];
     const randomSongs = document.getElementById('randomSongs');
-    
+    var URLS = [];
+    var e = document.getElementById('playlists'); //dropdown with all the playlists
+    var selectedPlaylist = e.selectedIndex;
+    console.log("Selected this index for random song", selectedPlaylist);
 
-    fetch('/get_playlist_URLS', {method: 'POST'}) // get Playlists URLS, and put them in a array.
+
+    fetch('/get_playlist_URLS', { method: 'POST' }) // get Playlists URLS, and put them in a array, then go through the array and find the right link.
         .then(response => response.json())
         .then(data => {
             data.forEach(URL => {
+                //console.log("URL", URL)
                 URLS.push(URL);
             });
-        
-        URLS.forEach(URL => { // for each url, get a random song. 
-            fetch('/getPlaylistSong', {
-                method: 'POST', 
-                headers: {
-                    'Content-Type' : 'application/json'
-                },
-                body: JSON.stringify({playlistURL: URL})
-            })
-            .then(response => response.json())
-            .then(songData => {
-                const listItem = document.createElement('li');
-                listItem.textContent = songData;
-                randomSongs.appendChild(listItem);
-                console.log("Random song from playlist:", songData);
-            })
-        })
-    });
+
+            // console.log("URL LENGTH", URLS.length);
+
+            for (let i = 0; i < URLS.length; i++) {
+                //console.log("i, selectedPlaylist", i, selectedPlaylist);
+                if (i == selectedPlaylist) {
+                    console.log("IT'S THIS LINK");
+                        fetch('/getPlaylistSong', {
+                            method: 'POST', 
+                            headers: {
+                                'Content-Type' : 'application/json'
+                            },
+                            body: JSON.stringify({playlistURL: URLS[i]})
+                        })
+                        .then(response => response.json())
+                        .then(songData => {
+                            console.log("Random song from playlist:", songData);
+                            // const listItem = document.createElement('li');
+                            // listItem.textContent = songData;
+                            // randomSongs.appendChild(listItem);
+
+                        })
+
+
+                }
+            }
+        });
 });
 
 
-function PrintOutSelected(){
-    console.log("sonOfABanker")
-    var e = document.getElementById('selectPlaylist'); //dropdown with all the playlists
-    var selectedPlaylist = e.selectedIndex;
-    var test = e[1].textContent;
-    console.log("Should be running ", test)
-    alert("Selected playist changed to", selectedPlaylist);
-    // console.log("you selected this playlist ", selectedPlaylist);
-    // https://stackoverflow.com/questions/15647295/javascript-seeting-selectedindex-to-be-blank-for-select-do-not-work
+
+function PrintOutSelected() {
+    var e = document.getElementById('playlists'); //dropdown with all the playlists
+    console.log("SELECTED INDEX", e.selectedIndex);
+    var selectedPlaylist = e.options[e.selectedIndex].textContent;
+    console.log("Selected playlist", selectedPlaylist);
+
 }
